e4ab65b1eb90ca1add1aaeeaab8646f8ec9d6028
deveshpatwa <deveshpatwa04@gmail.com>
2026-08-11 15:59:20 +0530

    change

--- Python Class Files/Py files/getting_data_from_googlesheets.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# just replace this if you want to get data from a different Google Sheet
-url = "https://docs.google.com/spreadsheets/d/196WXgqFuGi50mA5_8nYxUycwKKPwnRLE6gSUMQ2yCP8/edit?usp=sharing"
-
-url = url.replace("/edit?usp=sharing", "/export?format=csv")
-df = pd.read_csv(url)
-df.head()
