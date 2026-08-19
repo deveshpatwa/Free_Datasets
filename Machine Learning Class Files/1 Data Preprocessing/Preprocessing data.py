b0e3e352b0700ef83cf10a4ba6090ab9a311d3ef
deveshpatwa <deveshpatwa04@gmail.com>
2026-08-19 15:01:37 +0530

    change

--- Machine Learning Class Files/1 Data Preprocessing/Preprocessing data.py
+++ Machine Learning Class Files/1 Data Preprocessing/Preprocessing data.py
@@ -43,10 +43,10 @@ q3
 iqr = q3-q1
 iqr
 
-lb = q1-iqr*1.5 
+lb = q1-iqr1.5 
 lb
 
-ub = q3+iqr*1.5
+ub = q3+iqr1.5
 ub
 
 # this will filter the data 
@@ -69,6 +69,7 @@ df.describe().round(2)
 # Feature engineering is the process of using domain knowledge and statistical techniques to transform raw data into meaningful inputs (features) for machine learning models. Its goal is to highlight patterns and simplify data so algorithms can learn more effectively and make highly accurate predictions.
 
 # Adding columns like
+df.columns
 
 # - total rooms
 df['total_rooms'] = df.bedrooms + df.bathrooms 
@@ -77,4 +78,40 @@ df['total_rooms'] = df.bedrooms + df.bathrooms
 df['bathroom_per_bedroom'] = df.bathrooms / df.bedrooms
 
 # - total area of all floors
-df['aream_of_all_floors'] = df.area * df.stories
+df['aream_of_all_floors'] = df.area  df.stories
+
+# find num columns
+df.select_dtypes(include=int)
+
+df.select_dtypes(include=np.number)
+
+df.select_dtypes(include=str)
+
+
+# create a list of category and numeric columns
+num = df.select_dtypes(include=np.number).columns
+num
+
+cat = df.select_dtypes(include=str).columns
+cat
+
+df[num].corr()['price']
+
+df.head()
+
+
+# Scaling the data -------->
+
+# MinMaxScaler is a feature scaling technique that converts numerical values to a fixed range, usually 0 to 1, by subtracting the minimum value of the feature and dividing by the difference between the maximum and minimum values. This preserves the relative distances between data points while ensuring all features have the same scale. It is commonly used with machine learning algorithms such as KNN, K-Means, SVM, and Neural Networks, which are sensitive to the magnitude of input features. However, because it uses the minimum and maximum values, MinMaxScaler is sensitive to outliers.
+
+from sklearn.preprocessing import MinMaxScaler 
+
+df = pd.DataFrame({"income":[45,23,78,56,98,56,34,35]})
+print(df)
+
+scaler = MinMaxScaler()
+
+data = scaler.fit_transform(df)
+pd.DataFrame(data)  # scaled
+df                   # old data
+ 
